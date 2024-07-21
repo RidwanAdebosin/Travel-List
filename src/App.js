06120b4b85cc96e1523754a75c5cde47d596c73e
RidwanAdebosin <adebosinadewale@gmail.com>
2024-07-21 08:23:36 +0100

    updated

--- src/App.js
+++ src/App.js
@@ -1,46 +1,8 @@
-import { useState } from "react";
-import Logo from "./Logo";
-import Form from "./Form";
-import PackingList from "./PackingList";
-import Stats from "./Stats";
+import Logo from "./Logo"
 
-export default function App() {
-  const [items, setItems] = useState([]);
-
-  function handleAddItems(item) {
-    setItems((items) => [...items, item]);
-  }
-
-  function handleDeleteItems(id) {
-    setItems((items) => items.filter((item) => item.id !== id));
-  }
-
-  function handleToggleItem(id) {
-    setItems((items) =>
-      items.map((item) =>
-        item.id === id ? { ...item, packed: !item.packed } : item
-      )
-    );
-  }
-
-  function handleDeleteList() {
-    const confirmed = window.confirm(
-      "Are you sure you want to delete all items"
-    );
-    if (confirmed) setItems([]);
-  }
-
+const App = () => {
   return (
-    <div className="app">
-      <Logo />
-      <Form onAddItmes={handleAddItems} />
-      <PackingList
-        items={items}
-        onDeleteItem={handleDeleteItems}
-        onToggleItem={handleToggleItem}
-        handleDeleteList={handleDeleteList}
-      />
-      <Stats items={items} />
-    </div>
-  );
-}
+    <Logo/>
+  )
+};
+export default Logo;
